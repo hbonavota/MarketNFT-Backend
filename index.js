e5b0eaee7ef1b73184e95032e924dc71f34c074e
¨hbonavota¨ <hbonavota@gmail.com>
2021-09-16 11:59:28 +0200

    deploy server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("dotenv").config()
 const server = require('./app.js');
 require('./database');
+const port= process.env.port || 8001
 
 // Syncing all the models at once.
 
-  server.listen(8001, () => {
+  server.listen(port, () => {
     console.log('Running server: port 8001:'); // eslint-disable-line no-console
   });
 
